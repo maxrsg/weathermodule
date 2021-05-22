9805aa000c558224191fbcda944e3e643a1931c4
Max Gotenstam <maxrsg@gmail.com>
2021-05-22 11:42:05 +0100

    script works properly

--- .anax/scaffolding/postprocess.d/701_weathermodule.bash
+++ .anax/scaffolding/postprocess.d/701_weathermodule.bash
@@ -6,9 +6,10 @@
 #
 
 # Copy the configuration files
-rsync -av vendor/magm19/weathermodule/config/router ./
-rsync -av vendor/magm19/weathermodule/config/di/geoIp.php ./
-rsync -av vendor/magm19/weathermodule/config/di/weather.php ./
+rsync -av vendor/magm19/weathermodule/config/router/ ./config/router
+rsync -av vendor/magm19/weathermodule/config/api ./config/
+rsync -av vendor/magm19/weathermodule/config/di/geoIp.php ./config/di
+rsync -av vendor/magm19/weathermodule/config/di/weather.php ./config/di
 
 # Copy the src files
 rsync -av vendor/magm19/weathermodule/src ./
